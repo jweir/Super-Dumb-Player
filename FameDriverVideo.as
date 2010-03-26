a9e440201c1207b9f82fefa1d9a89f4391c9b07a
J. Weir <john@smokingun.com>
2010-03-26 15:22:56 -0400

    This is dumb

---
+++ FameDriverVideo.as
@@ -0,0 +1,178 @@
+//  F4V, MP4, M4A, MOV, MP4V, 3GP, and 3G2
+// TODO fix buffering update so it shows up even when the player is paused
+package {
+  import flash.display.LoaderInfo;
+  import flash.external.*; // woo
+  import flash.display.Sprite;
+	import flash.net.NetConnection;
+	import flash.net.NetStream;
+	import flash.media.Video;
+	import flash.media.SoundTransform;
+	import flash.display.Stage;
+  import flash.display.StageScaleMode;
+  import flash.display.StageAlign;
+  import flash.display.StageDisplayState;
+	import flash.events.NetStatusEvent;
+  import flash.utils.Timer;
+  import flash.events.TimerEvent;
+  
+  public class FameDriverVideo extends Sprite {
+
+    var video, 
+        stream, 
+        metaDataStore,
+        is_playing = false,
+        player_id,
+        timer:Timer;
+    
+    function FameDriverVideo(){
+      ExternalInterface.marshallExceptions = true;
+      
+      stage.scaleMode = StageScaleMode.NO_SCALE;
+      stage.align = StageAlign.TOP_LEFT;
+      
+      timer = new Timer(1000/30); // update 30 times a second
+  	  timer.addEventListener(TimerEvent.TIMER, whilePlaying);
+      
+      video = new Video();
+      var connection = new NetConnection();
+			connection.connect(null);
+      stream = new NetStream(connection);
+      
+  	  addChild(video);
+  	  publicMethods();
+  	  player_id = LoaderInfo(this.loaderInfo).parameters["target_id"];
+  	  
+  	  //ExternalInterface.call("video.initialized", player_id);
+  	  ExternalInterface.call("dumb_player.event", player_id, 'flashLoaded');
+    }
+    
+    function playState(state){
+      if(state){
+        timer.start();
+        is_playing = true;
+      } else {
+        timer.stop();
+        is_playing = false;
+      }
+      externalUpdateState();
+    }
+
+    function play(url){
+      if(stream){ stream.close(); }
+      playState(true)
+
+			stream.play(url, 10);
+			stream.client = {onMetaData: onMetaData};
+			video.attachNetStream(stream);
+			stream.addEventListener(NetStatusEvent.NET_STATUS, onNetStatus);
+    }
+    
+    function pause(){
+      playState(false);
+      stream.pause();
+      return is_playing;
+    }
+    
+    function resume(){
+      playState(true);
+      stream.resume();
+      return is_playing;
+    }
+    
+    function togglePlayPause(){
+      is_playing ? pause() : resume();
+    }
+    
+    // TODO add a method for when the movie has stopped
+    
+    // seeks either the time or a percentage, if the time is a string with a % '50%'
+    function seek(time){
+      if((time+"").match(/%/)){
+        time = parseFloat(time)/100 * metaDataStore.duration;
+      }
+      stream.seek(time);
+    }
+    
+    function publicMethods(){
+      ExternalInterface.addCallback("dumb_play",play);
+      ExternalInterface.addCallback("dumb_pause",pause);
+			ExternalInterface.addCallback("dumb_resume",resume);
+			ExternalInterface.addCallback("dumb_seek",seek);
+			ExternalInterface.addCallback("dumb_volume",volume);
+			ExternalInterface.addCallback("dumb_status",status);
+			ExternalInterface.addCallback("dumb_metaData",metaData);
+			ExternalInterface.addCallback("dumb_togglePlayPause", togglePlayPause)
+    }
+    
+    // click video to pause/resume
+    // callback to update status via timer
+
+    function status(){
+      return {
+        	bufferLength : stream.bufferLength,
+         	bufferTime : stream.bufferTime, 
+         	bytesLoaded :stream.bytesLoaded,
+         	bytesTotal : stream.bytesTotal
+      }
+    }
+        
+    function volume(){
+      if(arguments[0] !== undefined) stream.soundTransform = new SoundTransform(arguments[0]);
+      externalUpdateVolume();
+      return stream.soundTransform.volume;
+    }
+        
+    function metaData(){
+      return metaDataStore;
+		}
+    
+    function onMetaData(data){
+      metaDataStore = data;
+    }
+    
+    function onNetStatus(e){
+      trace(e.info.code);
+      
+      switch (e.info.code) {
+        case "NetStream.Seek.Notify":
+          externalUpdatePostion();
+          break;
+      	case "NetStream.Play.StreamNotFound":
+      		trace("Stream not found.");
+      	break;
+      	case "NetStream.Play.Stop":
+      	  trace("Movie is over")
+      	  playState(false);
+      	  break;
+      	break;
+      }
+			video.width = video.videoWidth;
+			video.height = video.videoHeight;
+		}
+		
+    function whilePlaying(e){
+      if(is_playing){
+        externalUpdatePostion();
+      }
+    }
+    
+    function externalUpdateVolume(){
+      ExternalInterface.call("dumb_player.event", player_id, 'videoVolume', (stream.soundTransform.volume > 0));
+    }
+    
+    function externalUpdateState(){
+      ExternalInterface.call("dumb_player.event", player_id, 'videoState', is_playing);
+    }
+    
+    function externalUpdatePostion(){
+      if(metaDataStore === undefined) return false;
+      ExternalInterface.call("dumb_player.event", player_id, 'videoUpdate', {
+        time      : stream.time,
+        duration  : metaDataStore.duration,
+        percentage: stream.time/metaDataStore.duration * 100,
+        loaded    : stream.bytesLoaded/stream.bytesTotal
+      });
+    }
+  }
+}
---
+++ video.js
@@ -0,0 +1,175 @@
+// TODO allow multiple players
+// TODO allow skinning
+// 177 loc
+
+(function(){
+
+  window.dumb_player = {
+    create      : create,
+    event       : function(id,event_name, data) {
+                    $("#"+id).trigger(event_name+"."+id, data)
+                  }
+  };
+
+  var player_source = "fdvideo.swf";
+
+  function create(id, video_src){
+    var flashvars   = {target_id:id},
+		    params      = {allowScriptAccess: "always", allowFullScreen:"true"},
+		    attributes  = {},
+        obj         = $("#"+id),
+        player      = function() {return $("#"+id)[0]},
+        original_state = obj.clone(true),
+        src         = video_src;
+
+    dumb_player.ui.create(obj);
+
+		swfobject.embedSWF(
+		    player_source,
+		    id,
+		    obj.width(),
+		    obj.height(),
+		    "9.0.0", null, flashvars, params, attributes);
+
+    var self = {
+        player : function(){ return player()},
+        play   : function(){ player().dumb_resume(); return self},
+        pause  : function(){ player().dumb_pause(); return self},
+        seek   : function(time){ player().dumb_seek(time); return self},
+        volume : function(value){ return player().dumb_volume(value) },
+        load   : function(src){ player().dumb_play(src); return self},
+        remove : function(){ $(player()).replaceWith(original_state);},
+        toggle : function(){ player().dumb_togglePlayPause(); return self;},
+        toggle_volume : function() {
+          (player().dumb_volume() > 0) ? player().dumb_volume(0) : player().dumb_volume(0.8);
+        }
+    }
+
+    $("#"+id).parent().bind("videoPlay."+id, self.play)
+      .bind("videoPause."+id, self.pause)
+      .bind("videoSeek."+id, function(e,d){self.seek(d)})
+      .bind("videoToggle."+id, self.toggle)
+      .bind("videoToggleVolume."+id, self.toggle_volume)
+      .bind("flashLoaded."+id, function(){ self.load(src).volume(); return self;});
+
+    return self;
+  }
+})();
+
+(function(){
+
+  window.dumb_player.ui = {
+    create  : create,
+    template :template,
+    time_formater : time_formater
+  };
+
+  var ui_template = $("\
+    <div class='dump_player_ui'>\
+      <div class='duration'/>\
+      <div class='time'/>\
+      <div class='scrubber'>\
+        <div class='track'>\
+          <div class='buffer'>\
+            <div class='thumb'/>\
+          </div>\
+        </div>\
+      </div>\
+      <div class='state'>\
+        <span class='play'>Pause</span>\
+        <span class='pause'>Play</span>\
+      </div>\
+      <div class='volume'>\
+        <span class='on'>Mute</span>\
+        <span class='off'>Unmute</span>\
+      </div>\
+    </div>");
+
+  function template(obj_or_html){
+    if(obj_or_html !== undefined) ui_template = $(obj_or_html);
+    return ui_template;
+  }
+
+  var time_formater_func = function(str){return parseInt(str*10)/10}
+
+  // Overwrite this function for formating the time
+  function time_formater(func){
+    if(func !== undefined) time_formater_func = func;
+    return time_formater_func;
+  }
+
+  function create(obj){
+    var id = obj.attr("id"),
+        ui = $("<div class='dumb_video_player' id='dumb_player_"+id+"'></div>"),
+        player_state,
+        container = obj.wrap(ui).parent();
+
+    container.append(ui_template.clone());
+    dumb_player.ui.scrubber.create(id, container);
+
+    container.find(".volume").click(function(){
+      dumb_player.event(id, "videoToggleVolume");
+    });
+
+    container.find(".state").click(function(){
+      dumb_player.event(id, "videoToggle");
+    });
+
+    container.bind("videoState."+id, function(event, state){
+      container.find(".state").find(".play, .pause").hide();
+      container.find(".state ."+ (state? "play":"pause")).show();
+    });
+
+    container.bind("videoVolume."+id, function(event, is_on){
+      container.find(".volume").find(".on, .off").hide();
+      container.find(".volume ."+ (is_on? "on":"off")).show();
+    });
+  }
+})();
+
+(function(){
+  window.dumb_player.ui.scrubber = {
+    create : create
+  }
+
+  function create(id, container){
+    var time      = container.find(".time"),
+        duration  = container.find(".duration"),
+        buffer    = container.find(".buffer"),
+        track     = container.find(".track"),
+        thumb     = container.find(".thumb"),
+        scrubber  = container.find(".scrubber"),
+        player_state,
+        is_dragging;
+
+    thumb.draggable({
+        start: function(){
+          is_dragging = true;
+          before_drag_state = player_state;
+          dumb_player.event(id, "videoPause")
+        },
+        drag: function(e, u){
+          dumb_player.event(id, "videoSeek", 100*(u.position.left/(track.width()-thumb.width()))+"%")
+        },
+        stop: function(){
+          is_dragging = false;
+          if(before_drag_state) dumb_player.event(id, "videoPlay");
+        },
+        containment: buffer,
+        scroll: false
+    });
+
+    container.bind("videoState."+id, function(e,s){player_state = s;})
+
+    container.bind("videoUpdate."+id, function(event, info){
+      time.text(dumb_player.ui.time_formater()(info.duration));
+      duration.text(dumb_player.ui.time_formater()(info.time));
+      buffer.css("width", info.loaded * track.width());
+      if(!is_dragging){
+        thumb.css("left", (info.percentage/100) * (track.width()-thumb.width()))
+      }
+    });
+
+    return scrubber;
+  }
+})();
